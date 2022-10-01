f00092d781025e94355aee6b30ad084ea460b6b6
estiben <estibenmontoyataborda@gmail.com>
2022-10-01 10:01:23 -0500

    bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,24 @@
 class BookingsController < ApplicationController
+
+  before_action :set_booking, only: :destroy
+  before_action :set_gams, only: [:new, :create]
+
+  def new
+    @booking = Boooking.new
+  end
+
+  def create
+    @booking = Bookings.new(booking_params)
+    @booking.game = @game
+  end
+
+  private
+
+  def set_bookings
+
+  end
+
+  def set_games
+
+  end
 end
