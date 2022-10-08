d0295daf70aba707fc7a0dd85273abec7cc78f7f
estiben <estibenmontoyataborda@gmail.com>
2022-10-08 08:24:14 -0500

    home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
-    @games = Game.all
+    @categories = Category.all
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_01_125243) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_08_132154) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,15 +25,23 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_01_125243) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image"
+  end
+
   create_table "games", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.float "price"
     t.string "image"
-    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_games_on_category_id"
     t.index ["user_id"], name: "index_games_on_user_id"
   end
 
@@ -55,5 +63,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_01_125243) do
 
   add_foreign_key "bookings", "games"
   add_foreign_key "bookings", "users"
+  add_foreign_key "games", "categories"
   add_foreign_key "games", "users"
 end
